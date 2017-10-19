2815fbf7e3d5ee30f8b2b2c9957028f2eac25779
DanielKell <danielkell.ubc@gmail.com>
2017-10-18 21:10:45 -0700

    Resolved circular references

--- users/Schema/schema.js
+++ users/Schema/schema.js
@@ -5,23 +5,31 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema //Takes in a root query, and returns a GraphQL Schema 
+    GraphQLSchema, //Takes in a root query, and returns a GraphQL Schema 
+    GraphQLList
 } = graphql;
 
 //must define above UserType
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({ //This POSTPONES the running of this until after the doc is loaded
         id: {type: GraphQLString},
         name: {type: GraphQLString},
-        description: {type: GraphQLString}
-    }
+        description: {type: GraphQLString},
+        users: {
+            type: new GraphQLList(UserType), //Telling GraphQL we will have multiple users
+            resolve(parentValue, args) {
+                return axios.get(`http.localhost:3000/companies/${parentValue.id}/users`)
+                .then (resp => resp.data);
+            }
+        }
+    })
 });
 
 //This object instructs graphql what a user object should look like
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
         age: {type: GraphQLInt},
@@ -33,7 +41,7 @@ const UserType = new GraphQLObjectType({
                 .then(resp => resp.data);
             }
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
