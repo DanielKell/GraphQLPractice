bb292c414e8f9f104f5e1e93c76498ca1570023f
DanielKell <danielkell.ubc@gmail.com>
2017-10-18 18:19:44 -0700

    Added dummy data and wrote RootQuery resolve logic

--- users/Schema/schema.js
+++ users/Schema/schema.js
@@ -1,10 +1,17 @@
 //Contains all of the knowledge about how each object should look/link to
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema //Takes in a root query, and returns a GraphQL Schema 
 } = graphql;
+
+const users = [
+    {id: '23', firstName: 'Bill', age: 20},
+    {id: '47', firstName: 'Samantha', age: 21}
+]
 
 //This object instructs graphql what a user object should look like
 const UserType = new GraphQLObjectType({
@@ -23,8 +30,13 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString} },
             resolve(parentValue, args){ //I'll look for the user!
-                
+                return _.find(users, {id: args.id }) 
+                //Look through users and return the id
             }
         } //args = arguments to return the corresponding user.
     } //If you give me an id, I will give you back the UserType you want
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
--- users/server.js
+++ users/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql'); //Glue between the two
+const schema = require('./Schema/schema');
 
 const app = express();
 
 //How we wire up middleware to express: app.use
 app.use('/graphql', expressGraphQL({
+    schema,
     graphiql: true
 })); 
 
